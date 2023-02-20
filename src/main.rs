1897e75652e773f1ac884e9ffe7acaaed9100140
Geoffrey Belcher <geoffreybelcher@icloud.com>
2023-02-20 11:06:15 -0400

    Added turn counter, user input for setting the range for the guessing game and the game is playable.

--- src/main.rs
+++ src/main.rs
@@ -3,32 +3,52 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
-
     // Set the range for the game.
-    
-    let low = 1;
-    let high = 10;
+    let mut low = String::new();
+    let mut high = String::new();
 
     println!("Welcome to Geoff's Guessing Game!");
-    println!("Please choose a range, lowest first")
 
+    // Set upper range.
+    println!("Choose your highest number (inclusive).");
+    io::stdin().read_line(&mut high).expect("Failed to read line");
+    let high: u32 = match high.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 1,
+    };
+
+    // Set lower range.
+    println!("Choose your lowest number (inclusive).");
+    io::stdin().read_line(&mut low).expect("Failed to read line");
+    let low: u32 = match low.trim().parse() {
+        Ok(num) => num,
+        Err(_) => 1,
+    };
+
+    // Initialize the global game variables.
     let secret_number = rand::thread_rng().gen_range(low..=high);
-    let mut guess = String::new();
+    let mut tries: u32 = 0;
 
     loop {
         println!("Guess a number between {} and {} (inclusive).", low, high);
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                continue;
+            }
+        };
+
+        tries += 1;
 
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("{} is too small!", guess),
             Ordering::Greater => println!("{} is too big!", guess),
             Ordering::Equal => {
-                println!("You win!");
+                println!("You win! The number was {}. It took you {} tries.", secret_number, tries);
                 break;
             }
         }
